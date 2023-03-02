19f9201e02f08a933c35e5986d5eea5b1af68280
shivanand0018 <101448498+shivanand0018@users.noreply.github.com>
2023-03-02 16:50:33 +0530

    routes js done

--- demo1.js
+++ demo1.js
@@ -1,29 +1,6 @@
 const http = require('http')
-const fs = require('fs');
 
-var server = http.createServer((req, res) => {
-    let url = req.url;
-    if (url === "/") {
-        const data = fs.readFileSync('file.txt', 'utf-8')
-        console.log(data);
-        res.write(`<html><head><h3>${data}</h3></head></html>`)
-        res.write('<html><body><form action="/message" method="POST"><label>Type the Message</label><input type="text" name="message"></input><button type="submit">Submit</button><form></body></html>')
-        return res.end();
-    }
-    if(url==="/message")
-    {
-        const body = []
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        })
-        req.on('end', () => {
-            const parsed = Buffer.concat(body).toString()
-            const message = parsed.split('=')[1];
-            console.log(message);
-            fs.writeFileSync('file.txt', message);
-            res.statusCode=302;
-            res.setHeader("Location","/")
-            res.end();
-        })
-    }
-}).listen('3000')
+const route = require('./routes');
+
+var server = http.createServer(route)
+server.listen('3000')
